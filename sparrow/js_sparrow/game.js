61e90187e7adcb1056372369e74a9274e2d7f453
blacksw <36705765+blacksw@users.noreply.github.com>
2018-10-17 22:56:52 +0300

    add die and sound

--- sparrow/js_sparrow/game.js
+++ sparrow/js_sparrow/game.js
@@ -3,7 +3,7 @@ var b = new Bird("yellow");
 b.render();
 b.fly();
 b.div.onclick = shoot;
-var shots = 0;
+var shots =15;
 var kills = 0;
 var kill = document.querySelector('#kills');
 var shot = document.querySelector('#shots');
@@ -16,12 +16,25 @@ b = new Bird(change[change_bird]);
 b.render();
 b.fly();
 b.div.onclick = shoot;
-
 kills++;
-shots++;
 kill.innerHTML = kills + " ";
-shot.innerHTML = shots + " ";
-console.log(shots);
+
+}
+
+function end(){
+	var game_over = document.createElement('div');
+	game_over.setAttribute('class','over');
+	var bod = document.getElementsByTagName('body');
+	document.body.appendChild(game_over);
+	var snd = new Audio("../images/gunshot.wav");
+    snd.play();
+}
+function count(){
+	--shots;
+	if(shots == 0){
+     end();
+ }
+    shot.innerHTML = shots + " ";	
 }
 
 // функцией shoot() при 1) найти птицу 2)setTimeout(function(){}) -> removeChild
